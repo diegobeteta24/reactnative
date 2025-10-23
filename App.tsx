c13158d023d14eb151215880ce396fffaf8f0242
unknown <HP@DESKTOP-OAI08BH.corp.local>
2025-10-23 12:19:27 -0600

    chore: update project files

--- App.tsx
+++ App.tsx
@@ -1,11 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, Touchable, TouchableOpacity } from 'react-native';
+import DefaultRoundedButton from './presentation/components/DefaultRoundedButton';
+import DefaultTextInput from './presentation/components/DefaultTextInput';
 
 export default function App() {
   return (
+
+    //LOGIN SCREEN
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.ImageBackground}
+        source={require('./assets/city.jpg')}
+      />
+
+      <View style={styles.form}>
+
+        <Image
+
+          source={require('./assets/user.png')}
+          style={styles.imageUser}
+        />
+
+        <Text style={styles.textLogin}>LOGIN</Text>
+
+
+           <DefaultTextInput
+             placeholder="Correo Electrónico"
+             value='hola'
+             onChangeText={text => {}}
+             icon={require('./assets/email.png')}
+             keyboardType='email-address'
+           />
+            <DefaultTextInput
+              placeholder="Contraseña"
+              value=''
+              onChangeText={text => {}}
+              icon={require('./assets/password.png')}
+             
+              secureTextEntry
+            />
+ 
+
+
+
+        <DefaultRoundedButton
+          text="Iniciar Sesión"
+          onPress={() => {}}
+        />
+
+        <View style={styles.RegisterContainer}>
+          <View style={styles.divider}></View>
+          <Text style={styles.textoRegistro}>¿No tienes cuenta?</Text>
+          <View style={styles.divider}></View>
+        </View>
+
+
+   <DefaultRoundedButton
+          text="Registrarse"
+          onPress={() => {}}
+          backgroundColor="black"
+        />
+
+
+
+      </View>
+
+
     </View>
   );
 }
@@ -13,8 +73,61 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ImageBackground: {
+    width: '100%',
+    height: '100%',
+    opacity: 0.7,
+  },
+  form: {
+    width: '87%',
+    height: '75%',
+    position: 'absolute',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    borderRadius: 40,
+    justifyContent: 'center',
+    paddingHorizontal: 25,
+  },
+  imageUser: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+    marginBottom: 15,
+  },
+  textLogin: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+
+  },
+ 
+  RegisterContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15,
+
+  },
+
+
+  textoRegistro: {
+    color: 'white',
+    fontSize: 18,
+
+  },
+
+
+
+
+  divider: {
+    height: 1,
+    flex: 1,
+    backgroundColor: 'white',
+    marginHorizontal: 5,
+  },
+
 });
