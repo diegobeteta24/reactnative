4d005ea259c9bedbd381c5cd4ae584bf24ed975b
unknown <HP@DESKTOP-OAI08BH.corp.local>
2025-10-27 12:14:27 -0600

    feat: include React Native project in monorepo

---
+++ IndriverReactNative/App.tsx
@@ -0,0 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
+
+import { MainStackNavigator } from './presentation/navigator/MainStackNavigator';
+
+
+export default function App() {
+  return (
+
+    <NavigationContainer>
+      <MainStackNavigator />
+    </NavigationContainer>
+    
+  );
+}
+
